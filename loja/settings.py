9f7c05709f562c94126bdf7cff327c34d094c792
Beverly Timóteo <beverlytimoteo@yahoo.com.br>
2022-03-25 21:04:39 -0300

    Carrinho de compras / uso de session

--- loja/settings.py
+++ loja/settings.py
@@ -41,8 +41,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-
-
+    'crispy_forms',
 
     # TODO: Remover debug toolbar
     'debug_toolbar'
